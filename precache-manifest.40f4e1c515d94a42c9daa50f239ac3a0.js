8f29b5743fe352442af085251772c44b4641b849
zoudingyi <541363353@qq.com>
2020-06-05 11:07:35 +0800

    Updates

---
+++ precache-manifest.40f4e1c515d94a42c9daa50f239ac3a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5228be647e8078711ed067fea14fc37c",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "b92d41d5ccec362d87c3",
+    "url": "/github-actions-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3046173cc2be5b2fba7e",
+    "url": "/github-actions-demo/static/js/2.81f34c35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-actions-demo/static/js/2.81f34c35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b92d41d5ccec362d87c3",
+    "url": "/github-actions-demo/static/js/main.9b932abb.chunk.js"
+  },
+  {
+    "revision": "b5c74e0d151a1c4978f5",
+    "url": "/github-actions-demo/static/js/runtime-main.2f497a14.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
